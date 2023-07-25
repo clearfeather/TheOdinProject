b771b55ecc2740bcb92ba61aca3e3d0bd348806e
clearfeather <info@clearfeather.com>
2023-07-24 20:25:36 -0500

    updated dom elements

--- JS Basics/Project-Rock-Paper-Scissors/index.js
+++ JS Basics/Project-Rock-Paper-Scissors/index.js
@@ -1,6 +1,13 @@
+// Rules: Rock beats Scissors, Paper beats Rock, Scissors beats Paper
+
+// Three Choices
 const choices = ['ROCK', 'PAPER', 'SCISSORS'];
 
-// Rules: Rock beats Scissors, Paper beats Rock, Scissors beats Paper
+
+// Set scores for both player and computer
+let playerScore = 0;
+let computerScore = 0;
+
 
 // Generate random computer choice
 function getComputerChoice() {
@@ -8,35 +15,30 @@ function getComputerChoice() {
 }
 
 
-// Get player selection function
-function getPlayerChoice() {
-    playerSelection = prompt('Rock, Paper, or Scissors?').toUpperCase();
-}
-
 // Play a Round function. Two params set for passthru
 function playRound(playerSelection, computerSelection) {
 
     // Checking Selections
 
-    // Check Tie
+    // Check if Tie
     if (playerSelection === computerSelection) {
         return `It's a tie`;
     }
-    
+
     // Player Wins
     if (playerSelection === 'ROCK' && computerSelection === 'SCISSORS') {
         playerScore++;
         return 'You Win, Rock smashes Scissors';
-    } 
+    }
     else if (playerSelection === 'PAPER' && computerSelection === 'ROCK') {
         playerScore++;
         return 'You Win, Paper covers Rock';
-    } 
+    }
     else if (playerSelection === 'SCISSORS' && computerSelection === 'PAPER') {
         playerScore++;
         return 'You Win, Scissors cuts Paper';
     }
-    
+
     // Computer Wins
     else if (computerSelection === 'ROCK' && playerSelection === 'SCISSORS') {
         computerScore++;
@@ -49,20 +51,30 @@ function playRound(playerSelection, computerSelection) {
     else if (computerSelection === 'SCISSORS' && playerSelection === 'PAPER') {
         computerScore++;
         return 'You Lose, Scissors cuts Paper';
-    } 
+    }
     else return `Please input a valid choice`;
 }
 
-// const playerSelection = getPlayerChoice();
+
+// Select DOM elements to update
+
+const player = document.querySelector('#player');
+const computer = document.querySelector('#computer');
+
+const playerChoice = document.querySelector('#playerChoice');
+const computerChoice = document.querySelector('#computerChoice');
+
+const playerCurrentScore = document.querySelector('#playerCurrentScore');
+const computerCurrentScore = document.querySelector('#computerCurrentScore');
 
 
-// Set scores for both player and computer
-let playerScore = 0;
-let computerScore = 0;
+// Add Event Listeners for buttons
+
+
+
 
 // Create game
 function game() {
-    
 
     // Loop 5 times
     for (let i = 1; i <= 5; i++) {
@@ -70,28 +82,19 @@ function game() {
         // Set random computer choice function to var to access via variable to call/run each time in loop
         const computerSelection = getComputerChoice();
 
-        // Bring in User and Computer choices
-        getComputerChoice();
-        getPlayerChoice();
+        // Set player selection variable
+        let playerSelection;
 
         // Display choices
-        console.log(`Computer's Choice: ${computerSelection}`);
-        console.log(`Player's Choice: ${playerSelection}`);
+        playerChoice.textContent = playerSelection;
+        computerChoice.textContent = computerSelection;
 
-        console.log();
+        // Display/Update scores
+        playerCurrentScore.textContent = playerScore;
+        computerCurrentScore.textContent = computerScore;
 
         // Call Playround in for loop
-        console.log(playRound(playerSelection, computerSelection));
-
-        console.log();
-
-        // Display scores
-        console.log(`Player Score: ${playerScore}`);
-        console.log(`Computer Score: ${computerScore}`);
-
-        console.log();
-        console.log('------------------------');
-        console.log();
+        playRound(playerSelection, computerSelection);
 
         if (i === 5) {
             console.log('Game Over');
@@ -102,4 +105,5 @@ function game() {
     }
 }
 
- game();
+// Run game
+game();
